650b92a9eeffebeaebcd08c02bd669d54c11c9ef
GSM-Godwin <godwinaliu29@gmail.com>
2024-06-06 22:05:02 +0100

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://usable-mutt-48.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
